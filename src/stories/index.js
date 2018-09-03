9a83c7b3adfc297a94ecf4e4233ba8d556df7b5c
Anthony Tran <anthonytran.dev@gmail.com>
2018-09-03 18:44:43 +0100

    dev stories introduced LocationType, ResultCard
    LocationType - was present to showcase 5 different varieties of LocationType labels
    
    ResultCard - was present to showcase 5 different Card types, each of them with minute differences.  This was necessary, to display the flexibility of the component given different endpoint data

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,6 +1,4 @@
-import React from 'react'
-import { configure, mount } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import React, { Fragment } from 'react'
 import expect from 'expect'
 
 import { storiesOf } from '@storybook/react'
@@ -8,8 +6,132 @@ import { specs, describe, it } from 'storybook-addon-specifications'
 import { action } from '@storybook/addon-actions'
 import { linkTo } from '@storybook/addon-links'
 import { Button, Welcome } from '@storybook/react/demo'
+import LocationType from './../components/LocationType'
+import ResultCard from './../components/ResultCard'
 
-configure({ adapter: new Adapter() })
+const centerStyles = {
+  height: '50vw',
+  width: '100%',
+  paddingTop: 50,
+  background: '#aaaaaa',
+  display: 'flex',
+  justifyContent: 'space-around',
+}
+
+const listStyles = {
+  container: {
+    height: '50vw',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    listStyleType: 'none',
+    padding: 0,
+    borderRadius: "10px",
+    background: '#aaaaaa',
+  },
+  wrapper: {
+    listStyleType: 'none',
+    padding: 0,
+  },
+  wrapperFullWidth: {
+    listStyleType: 'none',
+    padding: 0,
+    width: 620,
+  }
+}
+
+const CenterDecorator = (storyFn) => (
+  <div style={centerStyles}>
+    {storyFn()}
+  </div>
+);
+
+const ListDecorator = (storyFn) => (
+  <div style={listStyles.container}>
+    <ul style={listStyles.wrapperFullWidth}>
+      {storyFn()}
+    </ul>
+  </div>
+)
+
+storiesOf('LocationType', module)
+  .addDecorator(CenterDecorator)
+  .add('with all types of labels', () => {
+    return (
+      <Fragment>
+        <LocationType type="airport">Airport</LocationType>
+        <LocationType type="city">City</LocationType>
+        <LocationType type="district">District</LocationType>
+        <LocationType type="station">Station</LocationType>
+        <LocationType type="place">Place</LocationType>
+      </Fragment>
+    )
+  })
+
+storiesOf('ResultCard', module)
+  .addDecorator(ListDecorator)
+  .add('with only airport label', () => {
+    const props = {
+      city: "London",
+      country: "United Kingdom",
+      iata: "LHR",
+      name: "Heathrow Airport",
+      type: "airport",
+    }
+    return (
+      <Fragment>
+        <ResultCard {...props} />
+      </Fragment>
+    )
+  })
+  .add('with all the label types', () => {
+    const airportProps = {
+      city: 'London',
+      country: 'United Kingdom',
+      iata: 'LHR',
+      name: 'Heathrow Airport',
+      type: 'airport',
+      region: 'Greater London',
+    }
+    const cityProps = {
+      name: 'London',
+      country: 'United Kingdom',
+      type: 'city',
+      region: 'Greater London',
+    }
+    const trainProps = {
+      city: 'Rome',
+      country: 'Italy',
+      name: 'Rome - Termini Train Station',
+      type: 'train',
+      region: 'Lazio',
+    }
+    const placeProps = {
+      country: 'United State of America',
+      name: 'Manhattan',
+      type: 'place',
+      region: 'New York State',
+    }
+    const districtProps = {
+      country: 'Spain',
+      name: 'Mallorca',
+      type: 'district',
+    }
+    const ItemTypes = [
+      airportProps,
+      cityProps,
+      trainProps,
+      placeProps,
+      districtProps,
+    ].map(type => <ResultCard {...type} />)
+    return (
+      <div>
+        { ItemTypes }
+      </div>
+    )
+  })
+
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />)
 
@@ -37,5 +159,3 @@ storiesOf('Button', module)
 
   return story
 })
-
-storiesOf('SearchBar', module).add('blank component', () => <Welcome showApp={linkTo('Button')} />)
