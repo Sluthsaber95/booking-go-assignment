ed10bd8308ce31d54157bd3d9c1fccb935f1b15a
Anthony Tran <anthonytran.dev@gmail.com>
2018-09-03 22:08:00 +0100

    feat App init
    This container component follows the pattern of housing  all the logic and state in once place, this allows for a simpler architecture - makes understanding the entire app easier

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import SearchBar from './components/SearchBar'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      searchResults: [],
+      value: '',
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.getSearchResults = this.getSearchResults.bind(this)
+  }
+  handleChange(event){
+    this.setState({ value: event.target.value})
+    this.getSearchResults()
+  }
+  getSearchResults(){
+    axios.get(`https://cors.io/?https://www.rentalcars.com/FTSAutocomplete.do?`, {
+      params: {
+        'solrIndex': 'fts_en',
+        'solrRows': '6',
+        'solrTerm': this.state.value,
+      }
+    })
+    .then(response => {
+      if(response.status >= 200 && response.status < 400){
+        console.log(response.data.results.docs)
+        this.setState({ searchResults: response.data.results.docs })
+      }
+      return
+    })
+    .catch(error => { console.log(error) })
+  }
   render() {
+    const { searchResults, value } = this.state
+    const handleChange = this.handleChange
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SearchBar
+          handleChange={handleChange}
+          searchResults={searchResults}
+          value={value}
+        />
       </div>
     );
   }
