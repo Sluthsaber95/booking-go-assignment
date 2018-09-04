04a6d2794bad95a7a9010471ad132537551ff7b8
Anthony Tran <anthonytran.dev@gmail.com>
2018-09-04 09:59:41 +0100

    dev search_bar_contents.spec.js complete spec
    These tests follow the majority of the spec listed in TEST 1, parts that are not tested are either part of the browser default behaviour and tests that are currently optional.
    
    Solution may be written in the future, but are not mission critical

--- cypress/integration/search_bar_contents.spec.js
+++ cypress/integration/search_bar_contents.spec.js
@@ -1,7 +1,6 @@
 /*
 TODO
-May need to use enzyme perhaps or some way of getting access to the label,
-probably remove test if unable to do bdd
+- Change Global Stats
 */
 
 // Denoted as TEST 1 in the spec sheet
@@ -10,14 +9,21 @@ describe('Search Bar Contents', function () {
     cy.visit('http://localhost:3000/')
   })
   context('landing on rentalcars.com', () => {
-    it('shows a search widget')
-    it('shows a search widget labeled "Pick up location"')
+    it('shows a search widget', () => {
+      cy.get('.search-bar-container')
+    })
+    it('shows a search widget labeled "Pick up location"', () => {
+      cy.get('.search-bar-label')
+        .should('have.text', 'Pick-Up Location')
+    })
   })
   context('search-widget', () => {
-    it('shows an input field consist of placeholder text "city, airport, station, region and district..."')
-    it('highlights the input field when click on')
-    it('does not highlight the input field when clicked off')
-
-    it('contains an aria-label "Pick up location" as part of the input field')
+    it('shows an input field consist of placeholder text "city, airport, station, region and district..."', () => {
+      cy.get('.search-bar')
+        .should('have.text', '')
+      
+      cy.get('.search-bar')
+        .should('have.attr', 'placeholder', 'city, airport, station, region and district...')
+    })
   })
 })
