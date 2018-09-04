6bb551cbcfba49df726f931da4cea9c333bdc797
Anthony Tran <anthonytran.dev@gmail.com>
2018-09-04 13:58:40 +0100

    feat SearchBar no display of cards if single value present

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,20 +2,28 @@ import React from 'react'
 import './../styles/css/main.css'
 import ResultList from './ResultList'
 
-const SearchBar = ({ handleChange, searchResults, value }) => 
-  <section className="search-bar-container">
-    <article className="search-bar-wrapper">
-      <h2 className="search-bar-title">Where are you going?</h2>
-      <label className="search-bar-label">Pick-Up Location</label>
-      <input
-        className="search-bar"
-        placeholder="city, airport, station, region and district..."
-        onChange={handleChange}
-        value={value}
-        type="text"
-      />
-      <ResultList searchResults={searchResults} />
-    </article>
-  </section>
+const SearchBar = ({ handleChange, searchResults, value }) => {
+  const multipleCharactersEntered = value ? value.length > 1 : false
+  return (
+    <section className="search-bar-container">
+      <article className="search-bar-wrapper">
+        <h2 className="search-bar-title">Where are you going?</h2>
+        <label className="search-bar-label">Pick-Up Location</label>
+        <input
+          className="search-bar"
+          placeholder="city, airport, station, region and district..."
+          onChange={handleChange}
+          value={value}
+          type="text"
+        />
+        {
+          multipleCharactersEntered
+            ? <ResultList searchResults={searchResults} />
+            : <div></div>
+        }
+      </article>
+    </section>
+  )
+}
 
 export default SearchBar
